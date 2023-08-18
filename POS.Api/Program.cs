cfbc093d3ba7f91e975b761eefc84c21540b5222
carlos melgarejo <carlos melgarejo.solorzano@hotmail.com>
2023-08-18 17:57:29 -0500

    primer commit

---
+++ POS.Api/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+//dotnet ef dbcontext scaffold "Server=(local); DataBase=POS; Trusted_Connection=True;TrustServerCertificate=True;" Microsoft.EntityFrameworkCore.SqlServer --output-dir Entities
---
+++ POS.Infrastructure/Persistences/Contexts/PosContext.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace POS.Domain.Entities;
+
+public partial class PosContext : DbContext
+{
+    public PosContext()
+    {
+    }
+
+    public PosContext(DbContextOptions<PosContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<BranchOffice> BranchOffices { get; set; }
+
+    public virtual DbSet<Business> Businesses { get; set; }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Client> Clients { get; set; }
+
+    public virtual DbSet<Department> Departments { get; set; }
+
+    public virtual DbSet<District> Districts { get; set; }
+
+    public virtual DbSet<DocumentType> DocumentTypes { get; set; }
+
+    public virtual DbSet<Menu> Menus { get; set; }
+
+    public virtual DbSet<MenuRole> MenuRoles { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<Provider> Providers { get; set; }
+
+    public virtual DbSet<Province> Provinces { get; set; }
+
+    public virtual DbSet<Purcharse> Purcharses { get; set; }
+
+    public virtual DbSet<PurcharseDetail> PurcharseDetails { get; set; }
+
+    public virtual DbSet<Role> Roles { get; set; }
+
+    public virtual DbSet<Sale> Sales { get; set; }
+
+    public virtual DbSet<SaleDetail> SaleDetails { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    public virtual DbSet<UserRole> UserRoles { get; set; }
+
+    public virtual DbSet<UsersBranchOffice> UsersBranchOffices { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=(local); DataBase=POS; Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      
+       
+
+    
+
+     
+
+        modelBuilder.Entity<Provider>(entity =>
+        {
+            entity.HasKey(e => e.ProviderId).HasName("PK__Provider__B54C687D6016D84E");
+
+            entity.Property(e => e.DocumentNumber)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Email).HasMaxLength(255);
+            entity.Property(e => e.Phone).HasMaxLength(15);
+
+            entity.HasOne(d => d.DocumentType).WithMany(p => p.Providers)
+                .HasForeignKey(d => d.DocumentTypeId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Providers__Docum__5165187F");
+        });
+
+        modelBuilder.Entity<Province>(entity =>
+        {
+            entity.HasKey(e => e.ProvinceId).HasName("PK__Province__FD0A6F831D82A9B2");
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Department).WithMany(p => p.Provinces)
+                .HasForeignKey(d => d.DepartmentId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Provinces__Depar__52593CB8");
+        });
+
+        modelBuilder.Entity<Purcharse>(entity =>
+        {
+            entity.HasKey(e => e.PurcharseId).HasName("PK__Purchars__A98C674BAD73B483");
+
+            entity.Property(e => e.Tax).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.Total).HasColumnType("decimal(18, 2)");
+
+            entity.HasOne(d => d.Provider).WithMany(p => p.Purcharses)
+                .HasForeignKey(d => d.ProviderId)
+                .HasConstraintName("FK__Purcharse__Provi__5535A963");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Purcharses)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__Purcharse__UserI__5629CD9C");
+        });
+
+        modelBuilder.Entity<PurcharseDetail>(entity =>
+        {
+            entity.HasKey(e => e.PurcharseDetailId).HasName("PK__Purchars__7353248B569AD8D3");
+
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.PurcharseDetails)
+                .HasForeignKey(d => d.ProductId)
+                .HasConstraintName("FK__Purcharse__Produ__534D60F1");
+
+            entity.HasOne(d => d.Purcharse).WithMany(p => p.PurcharseDetails)
+                .HasForeignKey(d => d.PurcharseId)
+                .HasConstraintName("FK__Purcharse__Purch__5441852A");
+        });
+
+        modelBuilder.Entity<Role>(entity =>
+        {
+            entity.HasKey(e => e.RoleId).HasName("PK__Roles__8AFACE1A24F00F6D");
+
+            entity.Property(e => e.Description)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Sale>(entity =>
+        {
+            entity.HasKey(e => e.SaleId).HasName("PK__Sales__1EE3C3FFF83D6D16");
+
+            entity.Property(e => e.Tax).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.Total).HasColumnType("decimal(18, 2)");
+
+            entity.HasOne(d => d.Client).WithMany(p => p.Sales)
+                .HasForeignKey(d => d.ClientId)
+                .HasConstraintName("FK__Sales__ClientId__59063A47");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Sales)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__Sales__UserId__59FA5E80");
+        });
+
+        modelBuilder.Entity<SaleDetail>(entity =>
+        {
+            entity.HasKey(e => e.SaleDetailId).HasName("PK__SaleDeta__70DB14FE75ECC783");
+
+            entity.Property(e => e.Discount).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.SaleDetails)
+                .HasForeignKey(d => d.ProductId)
+                .HasConstraintName("FK__SaleDetai__Produ__571DF1D5");
+
+            entity.HasOne(d => d.Sale).WithMany(p => p.SaleDetails)
+                .HasForeignKey(d => d.SaleId)
+                .HasConstraintName("FK__SaleDetai__SaleI__5812160E");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PK__Users__1788CC4C79465CCF");
+
+            entity.Property(e => e.Email).IsUnicode(false);
+            entity.Property(e => e.Image).IsUnicode(false);
+            entity.Property(e => e.Password).IsUnicode(false);
+            entity.Property(e => e.UserName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<UserRole>(entity =>
+        {
+            entity.HasKey(e => e.UserRoleId).HasName("PK__UserRole__3D978A352597BC68");
+
+            entity.HasOne(d => d.BranchOffice).WithMany(p => p.UserRoles)
+                .HasForeignKey(d => d.BranchOfficeId)
+                .HasConstraintName("FK__UserRoles__Branc__5AEE82B9");
+
+            entity.HasOne(d => d.Role).WithMany(p => p.UserRoles)
+                .HasForeignKey(d => d.RoleId)
+                .HasConstraintName("FK__UserRoles__RoleI__5BE2A6F2");
+
+            entity.HasOne(d => d.User).WithMany(p => p.UserRoles)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__UserRoles__UserI__5CD6CB2B");
+        });
+
+        modelBuilder.Entity<UsersBranchOffice>(entity =>
+        {
+            entity.HasKey(e => e.UserBranchOfficeId).HasName("PK__UsersBra__7D1E804ACDC1D50C");
+
+            entity.HasOne(d => d.BranchOffice).WithMany(p => p.UsersBranchOffices)
+                .HasForeignKey(d => d.BranchOfficeId)
+                .HasConstraintName("FK__UsersBran__Branc__5DCAEF64");
+
+            entity.HasOne(d => d.User).WithMany(p => p.UsersBranchOffices)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__UsersBran__UserI__5EBF139D");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
